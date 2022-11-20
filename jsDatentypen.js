ae2a50ff2bcfef319142b83d987e06e283bd9a93
DaBorsten <boas.rautmann@gmail.com>
2022-11-20 17:50:11 +0100

    javascript teil

---
+++ jsDatentypen.js
@@ -0,0 +1,15 @@
+/******************
+Einfache Datentypen
+******************/
+
+// Strings bzw. Texte werden mit einfachen Gänsefüßchen geschrieben
+let job = 'Physiklehrer'
+
+// Integer sind ganze Zahlen
+let anzahlHausaufgaben = 1000
+
+// Floats sind Kommazahlen und werden mit einem . geschrieben
+let iq = 30.4
+
+// Boolean kann true oder false zugewiesen werden
+let gutInPhysik = false
---
+++ jsVariablen.js
@@ -0,0 +1,17 @@
+
+
+/********
+Variablen
+********/
+
+// Variablen sind "Pakete", die Informationen beinhalten
+
+// Variablen werden mit let deklariert
+let variable
+
+/* Konstanten sind readonly, der Wert kannalso
+nicht nochmal überschrieben werden */
+const konstante = 9.81
+
+// Variablen kann gleich ein Wert mit = zugewiesen werden
+let vairable2 = 'test'
