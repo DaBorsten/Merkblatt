882171fc424353a599a5d94a023e1273006d22f5
DaBorsten <boas.rautmann@gmail.com>
2022-12-10 21:00:53 +0100

    cache

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
 const cacheName = 'cache-v1'
 const resourcesToPrecache = [
     '/',
-    'index.html',
-    'style.css',
-    'script.js',
-    'htmlGrundstruktur.webp',
-    'htmlTags.webp',
-    'tabelle.webp',
-    'BMI If Statements HTML Teil.webp',
-    'BMI If Statements JS Teil.webp',
-    'if else.webp',
-    'JavaScriptVerlinken.webp',
-    'jsEinfacheDatentypen.webp',
-    'jsVariablen.webp',
-    'logische Operatoren.webp',
-    'quadratische Gleichung HTML.webp',
-    'quadratische Gleichung JS.webp',
-    'schrauben_html.webp',
-    'schrauben_js.webp'
+    '/Merkblatt/index.html',
+    '/Merkblatt/style.css',
+    '/Merkblatt/script.js',
+    '/Merkblatt/htmlGrundstruktur.webp',
+    '/Merkblatt/htmlTags.webp',
+    '/Merkblatt/tabelle.webp',
+    '/Merkblatt/BMI If Statements HTML Teil.webp',
+    '/Merkblatt/BMI If Statements JS Teil.webp',
+    '/Merkblatt/if else.webp',
+    '/Merkblatt/JavaScriptVerlinken.webp',
+    '/Merkblatt/jsEinfacheDatentypen.webp',
+    '/Merkblatt/jsVariablen.webp',
+    '/Merkblatt/logische Operatoren.webp',
+    '/Merkblatt/quadratische Gleichung HTML.webp',
+    '/Merkblatt/quadratische Gleichung JS.webp',
+    '/Merkblatt/schrauben_html.webp',
+    '/Merkblatt/schrauben_js.webp'
 ]
 
 self.addEventListener('install', event => {
