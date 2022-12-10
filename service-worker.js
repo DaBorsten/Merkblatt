ebed6fc5cd93822c74f09bd3ee1049560268be64
DaBorsten <boas.rautmann@gmail.com>
2022-12-10 21:33:45 +0100

    test

--- service-worker.js
+++ service-worker.js
@@ -3,28 +3,16 @@ const resourcesToPrecache = [
     '/',
     '/index.html',
     '/style.css',
-    '/script.js',
-    '/htmlGrundstruktur.webp',
-    '/htmlTags.webp',
-    '/tabelle.webp',
-    '/BMI If Statements HTML Teil.webp',
-    '/BMI If Statements JS Teil.webp',
-    '/if else.webp',
-    '/JavaScriptVerlinken.webp',
-    '/jsEinfacheDatentypen.webp',
-    '/jsVariablen.webp',
-    '/logische Operatoren.webp',
-    '/quadratische Gleichung HTML.webp',
-    '/quadratische Gleichung JS.webp',
-    '/schrauben_html.webp',
-    '/schrauben_js.webp'
+    '/script.js'
 ]
 
 self.addEventListener('install', event => {
     console.log('Service worker install event!')
-    caches.open(cacheName).then(cache => {
-        cache.addAll(resourcesToPrecache)
-    })
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(resourcesToPrecache)
+        })
+    )
 })
 
 
