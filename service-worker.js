5486b6656445e807f38b7d34303b5eb52ddf03a7
DaBorsten <boas.rautmann@gmail.com>
2022-12-10 21:04:18 +0100

    cache

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
 const cacheName = 'cache-v1'
 const resourcesToPrecache = [
     '/',
-    'https://daborsten.github.io/Merkblatt/index.html',
-    'https://daborsten.github.io/Merkblatt/style.css',
-    'https://daborsten.github.io/Merkblatt/script.js',
-    'https://daborsten.github.io/Merkblatt/htmlGrundstruktur.webp',
-    'https://daborsten.github.io/Merkblatt/htmlTags.webp',
-    'https://daborsten.github.io/Merkblatt/tabelle.webp',
-    'https://daborsten.github.io/Merkblatt/BMI If Statements HTML Teil.webp',
-    'https://daborsten.github.io/Merkblatt/BMI If Statements JS Teil.webp',
-    'https://daborsten.github.io/Merkblatt/if else.webp',
-    'https://daborsten.github.io/Merkblatt/JavaScriptVerlinken.webp',
-    'https://daborsten.github.io/Merkblatt/jsEinfacheDatentypen.webp',
-    'https://daborsten.github.io/Merkblatt/jsVariablen.webp',
-    'https://daborsten.github.io/Merkblatt/logische Operatoren.webp',
-    'https://daborsten.github.io/Merkblatt/quadratische Gleichung HTML.webp',
-    'https://daborsten.github.io/Merkblatt/quadratische Gleichung JS.webp',
-    'https://daborsten.github.io/Merkblatt/schrauben_html.webp',
-    'https://daborsten.github.io/Merkblatt/schrauben_js.webp'
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/htmlGrundstruktur.webp',
+    '/htmlTags.webp',
+    '/tabelle.webp',
+    '/BMI If Statements HTML Teil.webp',
+    '/BMI If Statements JS Teil.webp',
+    '/if else.webp',
+    '/JavaScriptVerlinken.webp',
+    '/jsEinfacheDatentypen.webp',
+    '/jsVariablen.webp',
+    '/logische Operatoren.webp',
+    '/quadratische Gleichung HTML.webp',
+    '/quadratische Gleichung JS.webp',
+    '/schrauben_html.webp',
+    '/schrauben_js.webp'
 ]
 
 self.addEventListener('install', event => {
