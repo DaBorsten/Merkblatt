249844dc411586421bf6e5ee1a17dddd697710e1
DaBorsten <boas.rautmann@gmail.com>
2022-12-10 19:39:50 +0100

    test

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,30 @@
 const cacheName = 'cache-v1'
 const resourcesToPrecache = [
     '/',
-    'index.html',
-    'style.css',
-    'script.js',
-    'htmlGrundstruktur.webp',
-    'htmlTags.webp',
-    'tabelle.webp',
-    'BMI If Statements HTML Teil.webp',
-    'BMI If Statements JS Teil.webp',
-    'if else.webp',
-    'JavaScriptVerlinken.webp',
-    'jsEinfacheDatentypen.webp',
-    'jsVariablen.webp',
-    'logische Operatoren.webp',
-    'quadratische Gleichung HTML.webp',
-    'quadratische Gleichung JS.webp',
-    'schrauben_html.webp',
-    'schrauben_js.webp',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/htmlGrundstruktur.webp',
+    '/htmlTags.webp',
+    '/tabelle.webp',
+    '/BMI If Statements HTML Teil.webp',
+    '/BMI If Statements JS Teil.webp',
+    '/if else.webp',
+    '/JavaScriptVerlinken.webp',
+    '/jsEinfacheDatentypen.webp',
+    '/jsVariablen.webp',
+    '/logische Operatoren.webp',
+    '/quadratische Gleichung HTML.webp',
+    '/quadratische Gleichung JS.webp',
+    '/schrauben_html.webp',
+    '/schrauben_js.webp',
 ]
 
 self.addEventListener('install', event => {
     console.log('Service worker install event!')
     event.waitUntil(
-        caches.open(cacheName)
-            .then(cache => {
-                return cache.addAll(resourcesToPrecache)
+        caches.open(cacheName).then(cache => {
+                cache.addAll(resourcesToPrecache)
                     .catch(error => {
                         console.log('Error while caching resources:', error)
                     })
